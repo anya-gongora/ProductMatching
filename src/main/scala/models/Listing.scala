c488c64931ea7a7df40bdcdd024b6b3ba8c7f24d
Anya Gongora <anyagongora@gmail.com>
2017-11-28 10:09:17 -0400

    create listing class

--- src/main/scala/models/Listing.scala
+++ src/main/scala/models/Listing.scala
@@ -1,5 +1,23 @@
 package models
 
-class Listing {
+import play.api.libs.json.Reads
+import play.api.libs.functional.syntax._
+import play.api.libs.json._
+case class Listing(title: String, manufacturer: String, currency: String, price: String)
 
+object Listing {
+  implicit val listingReads: Reads[Listing] = (
+    (JsPath \ "title").read[String] and 
+      (JsPath \ "manufacturer").read[String] and
+      (JsPath \ "currency").read[String] and 
+      (JsPath \ "price").read[String]
+  )(Listing.apply _)
+  
+  implicit val listingWrites: Writes[Listing] = (
+    (JsPath \ "title").write[String] and
+      (JsPath \ "manufacturer").write[String] and
+      (JsPath \ "currency").write[String] and
+      (JsPath \ "price").write[String]
+  )(unlift(Listing.unapply _))
+  
 }
