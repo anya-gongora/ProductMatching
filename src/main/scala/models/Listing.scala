1b55da87b329f025ef4067014fcbf7ef24e2bf16
Anya Gongora <anyagongora@gmail.com>
2017-11-28 12:37:16 -0400

    fix listing format

--- src/main/scala/models/Listing.scala
+++ src/main/scala/models/Listing.scala
@@ -3,21 +3,22 @@ package models
 import play.api.libs.json.Reads
 import play.api.libs.functional.syntax._
 import play.api.libs.json._
+
 case class Listing(title: String, manufacturer: String, currency: String, price: String)
 
 object Listing {
   implicit val listingReads: Reads[Listing] = (
-    (JsPath \ "title").read[String] and 
+    (JsPath \ "title").read[String] and
       (JsPath \ "manufacturer").read[String] and
-      (JsPath \ "currency").read[String] and 
+      (JsPath \ "currency").read[String] and
       (JsPath \ "price").read[String]
-  )(Listing.apply _)
-  
+    ) (Listing.apply _)
+
   implicit val listingWrites: Writes[Listing] = (
     (JsPath \ "title").write[String] and
       (JsPath \ "manufacturer").write[String] and
       (JsPath \ "currency").write[String] and
       (JsPath \ "price").write[String]
-  )(unlift(Listing.unapply _))
-  
+    ) (unlift(Listing.unapply _))
+
 }
