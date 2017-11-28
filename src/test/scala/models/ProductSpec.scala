98657be3e2c324e292076b17e650fdcb81fea186
Anya Gongora <anyagongora@gmail.com>
2017-11-28 12:39:08 -0400

    update product test

--- src/test/scala/models/ProductSpec.scala
+++ src/test/scala/models/ProductSpec.scala
@@ -1,24 +1,25 @@
 package models
 
-import models.Product.dateFormat
-import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
 import org.scalatest.FlatSpec
 import play.api.libs.json.Json
 
+
 class ProductSpec extends FlatSpec {
+  val dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZ"
   val formatter = DateTimeFormat.forPattern(dateFormat).withOffsetParsed()
 
-  "product json " should "transform into Product class" in {
-   val json = Json.parse("""
-      |{
-      |  "product_name":"Sony_Cyber-shot_DSC-W310",
-      |  "manufacturer":"Sony",
-      |  "model":"DSC-W310",
-      |  "family":"Cyber-shot",
-      |  "announced-date":"2010-01-06T19:00:00.000-05:00"
-      |}
-    """.stripMargin)
+  "Product json " should "transform into Product class" in {
+    val json = Json.parse(
+      """
+        |{
+        |  "product_name":"Sony_Cyber-shot_DSC-W310",
+        |  "manufacturer":"Sony",
+        |  "model":"DSC-W310",
+        |  "family":"Cyber-shot",
+        |  "announced-date":"2010-01-06T19:00:00.000-05:00"
+        |}
+      """.stripMargin)
 
     val product: Product = json.as[Product]
     assert(product.productName === "Sony_Cyber-shot_DSC-W310")
@@ -27,5 +28,4 @@ class ProductSpec extends FlatSpec {
     assert(product.family === "Cyber-shot")
     assert(product.announcedDate === formatter.parseDateTime("2010-01-06T19:00:00.000-05:00"))
   }
-
 }
