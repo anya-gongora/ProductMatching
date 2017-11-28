28c02a22ea3f66dbb08c06ae13c37d6c7eaae098
Anya Gongora <anyagongora@gmail.com>
2017-11-28 12:38:35 -0400

    create result class

--- src/main/scala/models/Result.scala
+++ src/main/scala/models/Result.scala
@@ -1,5 +1,19 @@
 package models
 
-class Result {
+import play.api.libs.json.{JsPath, Reads}
+import play.api.libs.functional.syntax._
+import play.api.libs.json._
 
+case class Result(productName: String, listings: Seq[Listing])
+
+object Result {
+  implicit val resultReads: Reads[Result] = (
+    (JsPath \ "product_name").read[String] and
+      (JsPath \ "listings").read[Seq[Listing]]
+    ) (Result.apply _)
+
+  implicit val resultWrites: Writes[Result] = (
+    (JsPath \ "product_name").write[String] and
+      (JsPath \ "listings").write[Seq[Listing]]
+    ) (unlift(Result.unapply _))
 }
